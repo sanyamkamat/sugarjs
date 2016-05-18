3b806dfaa48942639ab6b864cd97b6c0a32b6bde
Sanyam Kamat <sanyam@hopscotch.in>
2016-05-18 14:09:18 +0530

    add sugar functions for String such as reverse, ucfirst and ucwords

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 /**
  * Created by sanyam on 18/5/16.
  */
+"use strict";
+
+(function () {
+
+  String.prototype.reverse = function () {
+    return this ? this.split("").reverse().join("") : this;
+  };
+
+  String.prototype.ucfirst = function () {
+    if (this.length > 1) {
+      return this[0].toUpperCase() + this.substr(1);
+    }
+    return this.toUpperCase();
+  };
+
+  String.prototype.ucwords = function () {
+    var stringWords = this.split(" ");
+
+    if (stringWords.length > 1) {
+      return (stringWords.reduce(function (previousValue, currentValue) {
+        return previousValue + " " + currentValue.ucfirst();
+      })).ucfirst();
+    }
+    return stringWords[0].ucfirst();
+  };
+
+})();
